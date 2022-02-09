3888d46ec17b033f19480a76908393274ecf8934
Tatyana <samaya11@narod.ru@$>
2022-02-09 17:18:21 +0500

    4 module done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  const newUl = document.createElement('UL');
+
+  function createLi(firstName, lastName) {
+   const newLi = document.createElement('LI');
+   newLi.textContent = firstName + ' ' + lastName;
+   return newLi;
+  }
+
+  for (let friend of friends) {
+    newUl.append(createLi(friend.firstName, friend.lastName));
+  }
+  return newUl;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+
+  let tBody = table.tBodies[0];
+
+  for (let i = 0; i < tBody.rows.length; i++) {
+    if(tBody.rows[i].cells[3].dataset.available == 'true') {
+      tBody.rows[i].classList.add('available');
+    } else if(tBody.rows[i].cells[3].dataset.available == 'false') {
+      tBody.rows[i].classList.add('unavailable');
+    } else {
+      tBody.rows[i].setAttribute('hidden', 'true');
+    }
+
+    if(tBody.rows[i].cells[2].textContent === 'm') {
+      tBody.rows[i].classList.add('male');
+    }
+
+    if (tBody.rows[i].cells[2].textContent === 'f') {
+      tBody.rows[i].classList.add('female');
+    }
+
+    if(Number(tBody.rows[i].cells[1].textContent) < 18) {
+      tBody.rows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
