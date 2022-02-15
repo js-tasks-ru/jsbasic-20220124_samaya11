bb0655e81f9be8c1ea076c4bc538d5d52adff823
Tatyana <samaya11@narod.ru@$>
2022-02-16 00:37:59 +0500

    5 madule done

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,52 @@
 function initCarousel() {
-  // ваш код...
+  const buttonRight = document.querySelector('.carousel__arrow_right');
+  const buttonLeft = document.querySelector('.carousel__arrow_left');
+  const elem = document.querySelector('.carousel__inner');
+  let width = document.querySelector('.carousel__slide').offsetWidth;
+  let countSlide = 1;
+  let slideOffset;
+
+  buttonLeft.style.display = 'none';
+
+  function goToRightSlide() {
+
+    slideOffset = width * countSlide;
+
+
+    elem.style.transform = `translateX(-${slideOffset}px)`;
+    countSlide++;
+
+    if (countSlide == 4) {
+      buttonRight.style.display = 'none';
+    }
+
+    else {
+      buttonLeft.style.display = 'flex';
+    }
+
+  }
+
+  function goToLeftSlide() {
+
+    slideOffset -= width;
+
+    elem.style.transform = `translateX(-${slideOffset}px)`;
+    countSlide--;
+
+    if (countSlide == 1) {
+      buttonLeft.style.display = 'none';
+    } else {
+      buttonRight.style.display = 'flex';
+    }
+
+  }
+
+
+  buttonRight.addEventListener('click', goToRightSlide);
+  buttonLeft.addEventListener('click', goToLeftSlide);
+
+
+
+
+
 }
