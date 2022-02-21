eb6458a99ecff97a2ce614a412f7f69e150e2eba
Tatyana <samaya11@narod.ru@$>
2022-02-21 11:49:04 +0500

    6 module is done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,58 @@
  *
  */
 export default class UserTable {
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+
+
   }
+
+  createRow(userName, userAge,userSalary,userCity) {
+    const row = document.createElement('tr');
+    row.insertAdjacentHTML('beforeend',
+    `<th style="border: 1px solid black">${userName}</th>
+    <th style="border: 1px solid black">${userAge}</th>
+    <th style="border: 1px solid black">${userSalary}</th>
+    <th style="border: 1px solid black">${userCity}</th>
+    <th style="border: 1px solid black"><button class="remove-button">x</button></th>`);
+    return row;
+
+  }
+
+  render() {
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    thead.insertAdjacentHTML('afterbegin',
+    `<tr>
+    <th>Имя</th>
+    <th>Возраст</th>
+    <th>Зарплата</th>
+    <th>Город</th>
+    <th></th>
+    </tr>`);
+    table.append(thead);
+    table.append(tbody);
+
+
+
+    for (const row of this.rows) {
+      tbody.append(this.createRow(row.name, row.age, row.salary, row.city));
+    }
+
+    table.addEventListener('click', this.onClick);
+
+    return table;
+  }
+
+  onClick(event) {
+    if (event.target.className != 'remove-button') return;
+
+    let pane = event.target.closest('tr');
+    pane.remove();
+  };
+
+
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,47 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.elem = this.render();
+  }
+
+  render() {
+    const newElem = createElement(`
+    <div class="card">
+    <div class="card__top">
+        <img class="card__image" alt="product">
+        <span class="card__price"></span>
+    </div>
+    <div class="card__body">
+        <div class="card__title"></div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+</div>
+`);
+
+const image = newElem.querySelector('.card__image');
+image.src = '/assets/images/products/' + this.product.image;
+
+const price = this.product.price.toFixed(2);
+const priceValue = newElem.querySelector('.card__price');
+priceValue.textContent = '€' + price;
+
+const cardTitle = newElem.querySelector('.card__title');
+cardTitle.textContent = this.product.name;
+
+const productAddEvent = new CustomEvent("product-add", {
+  detail: this.product.id,
+  bubbles: true
+});
+
+const cardButton = newElem.querySelector('.card__button');
+
+cardButton.addEventListener('click', (event) => newElem.dispatchEvent(productAddEvent));
+
+return newElem;
+
   }
 }
