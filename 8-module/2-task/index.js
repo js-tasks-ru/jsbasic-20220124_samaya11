15351e93e6a5eb554e3a2ea745606430d97eb378
Tatyana <samaya11@narod.ru@$>
2022-03-29 21:45:15 +0500

    module 8 1-2 test done

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -8,6 +8,17 @@ export default class ProductGrid {
     this.elem = this.render();
   }
 
+  createCard(product) {
+    let newCard = new ProductCard(product);
+      let newElem = newCard.elem;
+      newElem.dataset.nuts = product['nuts'];
+      newElem.dataset.vegeterian = product['vegeterian'];
+      newElem.dataset.spiciness = product['spiciness'];
+      newElem.dataset.categoryCard = product['category'];
+
+      return newElem;
+  }
+
   render() {
     let elem = createElement(`
     <div class="products-grid">
@@ -18,84 +29,88 @@ export default class ProductGrid {
     `);
 
     for (let product of this.products) {
-      let newCard = new ProductCard(product);
-      let newElem = newCard.elem;
-      newElem.dataset.nuts = product['nuts'];
-      newElem.dataset.vegeterian = product['vegeterian'];
-      newElem.dataset.spiciness = product['spiciness'];
-      newElem.dataset.categoryCard = product['category'];
-      elem.querySelector('.products-grid__inner').append(newElem);
-
+      elem.querySelector('.products-grid__inner').append(this.createCard(product));
     }
     return elem;
   }
 
   updateFilter(filters) {
 
-    for (let key in filters) {
-      this.filters[key] = filters[key];
-    }
+    Object.assign(this.filters, filters);
 
-    for (let card of this.elem.querySelectorAll('.card')) {
-      for (let key in this.filters) {
-        /////////////no nuts///////////
-        if (key == "noNuts") {
+    // for (let key in filters) {
+    //   this.filters[key] = filters[key];
+    // }
 
-
-          if (this.filters[key] == true && card.dataset.nuts == 'true') {
-
-            card.style.display = 'none';
-            break;
+    // for (let card of this.elem.querySelectorAll('.card')) {
+    //   for (let key in this.filters) {
+    //     /////////////no nuts///////////
+    //     if (key == "noNuts") {
 
 
-          } else {
-            card.style.display = '';
-          }
-        }
+    //       if (this.filters[key] == true && card.dataset.nuts == 'true') {
 
-        /////////////vegeterianOnly///////////
-
-        if(key == "vegeterianOnly") {
-          if (this.filters[key] == true && !(card.dataset.vegeterian == 'true')) {
-            card.style.display = 'none';
-            break;
-
-          } else {
-           card.style.display = '';
-          }
-        }
-
-      /////////////maxSpiciness///////////
-
-      if(key == "maxSpiciness") {
-        if (this.filters[key] >= card.dataset.spiciness) {
-          card.style.display = '';
-
-        } else {
-
-         card.style.display = 'none';
-          break;
-        }
-      }
-
-      if(key == "category") {
-        if (this.filters[key] == card.dataset.categoryCard || this.filters[key]== '') {
-          card.style.display = '';
-
-        } else {
-          card.style.display = 'none';
-          break;
-
-        }
-      }
+    //         card.style.display = 'none';
+    //         break;
 
 
-      }
+    //       } else {
+    //         card.style.display = '';
+    //       }
+    //     }
+
+    //     /////////////vegeterianOnly///////////
+
+    //     if(key == "vegeterianOnly") {
+    //       if (this.filters[key] == true && !(card.dataset.vegeterian == 'true')) {
+    //         card.style.display = 'none';
+    //         break;
+
+    //       } else {
+    //        card.style.display = '';
+    //       }
+    //     }
+
+    //   /////////////maxSpiciness///////////
+
+    //   if(key == "maxSpiciness") {
+    //     if (this.filters[key] >= card.dataset.spiciness) {
+    //       card.style.display = '';
+
+    //     } else {
+
+    //      card.style.display = 'none';
+    //       break;
+    //     }
+    //   }
+
+    //   if(key == "category") {
+    //     if (this.filters[key] == card.dataset.categoryCard || this.filters[key]== '') {
+    //       card.style.display = '';
+
+    //     } else {
+    //       card.style.display = 'none';
+    //       break;
+
+    //     }
+    //   }
 
 
-    }
+    //   }
 
-  }
+    //}
+
+    this.elem.querySelector(".products-grid__inner").innerHTML = "";
+
+   this.products.map(x => this.createCard(x))
+   .filter(x => !(this.filters.noNuts == true && x.dataset.nuts == 'true') || (!!!this.filters.noNuts))
+   .filter(x => (this.filters.vegeterianOnly == true && x.dataset.vegeterian == 'true') || (!!!this.filters.vegeterianOnly))
+   .filter(x => this.filters.maxSpiciness >= x.dataset.spiciness  || (!!!this.filters.maxSpiciness))
+   .filter(x => this.filters.category == x.dataset.categoryCard || !(this.filters.category))
+   .forEach(x =>  this.elem.querySelector(".products-grid__inner").append(x));
+
+
+ }
 
 
 
